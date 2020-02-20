74540440ca2384b06e500c4b1dbe840be63f311a
laveosa <laveosa@yahoo.com>
2020-02-20 21:38:21 +0200

    Initial commit

--- app.js
+++ app.js
@@ -1,32 +1,14 @@
 const dotenv = require("dotenv").config();
+const uuid = require("uuid");
 const http = require("http");
-const fs = require("fs");
-const ejs = require("ejs");
-const path = require("path");
 
-const PORT = process.env.PORT || "3000";
+const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.statusCode = 200;
-    res.setHeader = { "Content-Type": "text/html" };
-
-    fs.readFile(
-      path.join(__dirname, "app/server/views/home.ejs"),
-      "utf-8",
-      (err, data) => {
-        if (err) throw err;
-
-        let model = {
-          title: "EJS main page titile"
-        };
-
-        res.end(ejs.render(data, model));
-      }
-    );
-  }
-});
-
-server.listen(PORT, () => {
-  console.log("server is runing...");
-});
+const server = http
+  .createServer((req, res) => {
+    if (req.url === "/") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end("./app/index.html");
+    }
+  })
+  .listen(PORT);
