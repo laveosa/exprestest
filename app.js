7ed3cba3278165efc8455cfc72f66158b98639f0
laveosa <laveosa@yahoo.com>
2020-02-23 20:13:16 +0200

    Initial commit

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT;
 const server = http
   .createServer((req, res) => {
     if (req.url === "/") {
+      console.log(req.params);
+
       res.writeHead(200, { "Content-Type": "text/html" });
       res.end("./app/index.html");
     }
