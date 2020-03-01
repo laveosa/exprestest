59d8ad50ca0b96c1b58aedeb6ed42537bdacca30
laveosa <laveosa@yahoo.com>
2020-03-01 17:18:52 +0200

    Create - navigation block

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ const Config = require("./app/server/classes/Config");
 const PORT = process.env.PORT || 3000;
 const myConfig = new Config();
 let file = new static.Server("./app/client");
+let contentType = "text/html";
+let model = {
+  nav: {
+    url: null
+  }
+};
 
 http
   .createServer((req, res) => {
@@ -23,15 +29,45 @@ http
     }
 
     if (req.url === "/home") {
-      let contentType = myConfig.getContentType(req);
+      contentType = myConfig.getContentType(req);
 
       res.writeHead(200, {
         "Content-Type": contentType
       });
 
-      ejs.renderFile("./app/server/views/home.ejs", {}, (err, data) => {
+      model.nav.url = "/home";
+
+      ejs.renderFile("./app/server/views/home.ejs", model, (err, data) => {
         if (err) throw err;
 
+        res.end(data);
+      });
+    }
+
+    if (req.url === "/file-tree") {
+      contentType = myConfig.getContentType(req);
+
+      res.writeHead(200, {
+        "Content-Type": contentType
+      });
+
+      model.nav.url = "/file-tree";
+
+      ejs.renderFile("./app/server/views/file-tree.ejs", model, (err, data) => {
+        res.end(data);
+      });
+    }
+
+    if (req.url === "/log") {
+      contentType = myConfig.getContentType(req);
+
+      res.writeHead(200, {
+        "Content-Type": contentType
+      });
+
+      model.nav.url = "/log";
+
+      ejs.renderFile("./app/server/views/log.ejs", model, (err, data) => {
         res.end(data);
       });
     }
