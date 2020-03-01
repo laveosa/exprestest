81c5914618536120e33f60eaafdf5ff61d1e78b8
laveosa <laveosa@yahoo.com>
2020-03-01 15:55:37 +0200

    Create - class 'Config' transfer logic of 'set event listeners' and 'get context type' functions

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const http = require("http");
 const ejs = require("ejs");
 const static = require("node-static");
 
+const Config = require("./app/server/classes/Config");
+
 const PORT = process.env.PORT || 3000;
-var file = new static.Server("./app/client");
+const myConfig = new Config();
+let file = new static.Server("./app/client");
 
 http
   .createServer((req, res) => {
-    setEventListeners(req, res);
+    myConfig.setEventListeners(req, res, file);
 
     if (req.url === "/") {
       res.writeHead(302, {
@@ -20,7 +23,7 @@ http
     }
 
     if (req.url === "/home") {
-      let contentType = getContentType(req);
+      let contentType = myConfig.getContentType(req);
 
       res.writeHead(200, {
         "Content-Type": contentType
@@ -34,36 +37,3 @@ http
     }
   })
   .listen(PORT, err => {});
-
-function getContentType(req) {
-  let ext = path.extname(req.url);
-  let contenType = "text/html";
-
-  switch (ext) {
-    case ".txt":
-      contenType = "text/plain";
-      break;
-    case ".json":
-      contenType = "aplication/json";
-      break;
-    case ".js":
-      contenType = "text/javascript";
-      break;
-    case ".css":
-      contenType = "text/css";
-      break;
-    case ".png":
-      contenType = "image/png";
-      break;
-  }
-
-  return contenType;
-}
-
-function setEventListeners(req, res) {
-  req
-    .addListener("end", function() {
-      file.serve(req, res);
-    })
-    .resume();
-}
